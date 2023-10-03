91a64c0b1ca3a1b1f713749ecb1bb78c096a1653
optimisme <optimisme@gmail.com>
2023-10-03 21:12:00 +0200

    Fixed casting different sub classes

--- Project/src/Cercle.java
+++ Project/src/Cercle.java
@@ -19,7 +19,14 @@ public class Cercle extends Poligon {
 
     @Override
     public boolean equals(Object object2) {
+        // Comprova si són la mateixa referència
+        if (this == object2) return true;
+
         if (!(object2 instanceof Poligon) || !super.equals(object2)) return false;
+
+        // Comprova si els dos objectes són exactament de la mateixa classe
+        if (!this.getClass().equals(object2.getClass())) return false;
+
         Cercle cast2 = (Cercle) object2;
         return cast2.radius == radius;
     }
--- Project/src/Rectangle.java
+++ Project/src/Rectangle.java
@@ -20,7 +20,14 @@ public class Rectangle extends Poligon {
 
     @Override
     public boolean equals(Object object2) {
+          // Comprova si són la mateixa referència
+        if (this == object2) return true;
+        
         if (!(object2 instanceof Poligon) || !super.equals(object2)) return false;
+    
+        // Comprova si els dos objectes són exactament de la mateixa classe
+        if (!this.getClass().equals(object2.getClass())) return false;
+    
         Rectangle cast2 = (Rectangle) object2;
         return cast2.width == width && cast2.height == height;
     }
